d6beee2ba32c470855ad8b709fe588e683bd7869
mrjadaml <mrjadaml@gmail.com>
2017-02-06 11:42:04 -0800

    base setup for address comp, styling pending style guide

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -5,15 +5,10 @@ const Form = styled.form`
   display: grid;
   grid-template-columns: 50% 50%;
   grid-gap: 10px;
-
-  padding: 20px;
-  border: 1px solid black;
 `;
 
 const Input = styled.input`
-  margin-right: 10px;
-  padding: 10px;
-  background-color: #edf4e8;
+  background-color: #F1F1F1;
   display: block;
   width: 100%;
 `;
@@ -21,25 +16,9 @@ const Input = styled.input`
 const Select = styled.select`
   display: block;
   width: 100%;
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  appearance: none;
-
-  background-repeat: no-repeat;
-  background-size: .85em auto;
-  background-position: right 0.6em center;
-  padding: 10px;
-  border-radius: 0;
-  border: 0;
-  background-color: #edf4e8;
-  background-image: url("data:image/svg+xml;charset=utf-8, \
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 40'> \
-      <polygon points='0,0 60,0 30,40' style='fill:black;'/> \
-    </svg>");
 `;
 
 const Option = styled.option`
-  padding: 10px;
 `;
 
 const Label = styled.label`
@@ -50,9 +29,9 @@ class Address extends React.Component {
   render() {
     return (
       <Form>
-        <h3>Address</h3>
+        <h3>Address Component</h3>
         <Label fullwidth >Street 1:
-          <Input type="text" placeholder="400 Broad St" fullwidth />
+          <Input type="text" fullwidth />
         </Label>
 
         <Label fullwidth >Street 2:
@@ -65,8 +44,9 @@ class Address extends React.Component {
 
         <Label>State:
           <Select>
-            <Option value="bob">Bob</Option>
             <Option value="foo">Foo</Option>
+            <Option value="bar">Bar</Option>
+            <Option value="baz">Baz</Option>
           </Select>
         </Label>
 

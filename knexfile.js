0db8dd629a608f006e94bb61d1c6140befe80360
Tyler Miller <tylermillerhome@gmail.com>
2017-02-06 16:59:04 -0800

    Completed requested changes

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,11 @@
-'use strict';
-
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/git-paid'
+    connection: 'postgres://localhost/git-paid',
   },
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
-  }
+    connection: process.env.DATABASE_URL,
+  },
 };

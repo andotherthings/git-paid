776cbf1881f9489d09aa15dc0cedf83af202e613
THACHER <thachert1d@gmail.com>
2017-02-21 18:19:53 -0800

    landing page component design and update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import JobForm from './JobForm';
-import Header from './Header';
+import Landing from './Landing';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>App Component</h1>
+        <Landing />
         <JobForm />
       </div>
     );

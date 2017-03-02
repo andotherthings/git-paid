fe03febc9bba22fb3a3e466646567c0f3a4c9803
mrjadaml <mrjadaml@gmail.com>
2017-03-02 13:53:23 -0800

    dashboard rows (pending API endpoint) ✋

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,65 +1,135 @@
 import React from 'react';
 import { Grid, Cell } from 'radium-grid';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Button from './Button';
 import FormGroup from './FormGroup';
+import Tag from './Tag';
 
-const Dashboard = () => {
-  const { styles } = Dashboard;
+class Dashboard extends React.Component {
+  constructor() {
+    super()
 
-  return (
-    <section style={styles.section}>
-      <Grid width="1/4">
-        <Cell align="left">
-          <FormGroup
-            type="text"
-            label="search"
-            placeholder="what are you looking for?" />
-        </Cell>
+    this.state = {
+      jobApps: [
+        {
+          companyName: 'Space x',
+          jobTitle: 'Rocket Scientist',
+          industries: [
+            'Space',
+            'Rockets',
+            'Satellites'
+          ],
+        }, {
+          companyName: 'Planetary Resources',
+          jobTitle: 'Space Data Miner',
+          industries: [
+            'Space',
+            'Rocks',
+          ],
+        }, {
+          companyName: 'Thought Bot',
+          jobTitle: 'Googler',
+          industries: [
+            'SaaS',
+            'Consultancy',
+          ],
+        }
+      ],
+    }
+  }
 
-        <Cell align="right" width="3/4">
-          <Link to='/new-job'>
-            <Button type="info">New Job</Button>
-          </Link>
-        </Cell>
-      </Grid>
+  componentDidMount() {
+    axios.get('SOME_ENDPOINT')
+      .then(res => {
+        console.log('MISSING API ENDPOINT', res);
 
-      <header style={styles.header}>
-        <Grid width="1/3">
-          <Cell>
-            Company Name
+        //const nextState;
+        //this.setState(nextState)
+      })
+      .catch(err => {
+        console.log('ERR', err);
+      });
+  }
+
+  render() {
+    const jobApps = this.state.jobApps.map((jobApp, jobIndex) => {
+      const industries = jobApp.industries.map((industry, tagIndex) => {
+        return (
+          <Tag
+            key={ tagIndex } >
+              { industry }
+          </Tag>
+        )
+      });
+
+      return (
+        <Link
+          to={`/job/${jobIndex}`}
+          key={ jobIndex } >
+
+          <article
+            style={styles.tableRow}>
+
+            <Grid width="1/3">
+              <Cell>
+                { jobApp.companyName }
+              </Cell>
+
+              <Cell>
+                { jobApp.jobTitle }
+              </Cell>
+
+              <Cell>
+                { industries }
+              </Cell>
+            </Grid>
+          </article>
+        </Link>
+      );
+    });
+
+    return (
+      <section style={styles.section}>
+        <Grid width="1/4">
+          <Cell align="left">
+            <FormGroup
+              type="text"
+              label="search"
+              placeholder="what are you looking for?" />
           </Cell>
 
-          <Cell>
-            Job Title
-          </Cell>
-
-          <Cell>
-            Industries
+          <Cell align="right" width="3/4">
+            <Link to='/new-job'>
+              <Button type="info">New Job</Button>
+            </Link>
           </Cell>
         </Grid>
-      </header>
 
-      <article style={styles.tableRow}>
-        <Grid width="1/3">
-          <Cell>
-            SpaceX
-          </Cell>
+        <header style={styles.header}>
+          <Grid width="1/3">
+            <Cell>
+              Company Name
+            </Cell>
 
-          <Cell>
-            Software Engineer
-          </Cell>
+            <Cell>
+              Job Title
+            </Cell>
 
-          <Cell>
-            [Space] [IoT] [Communication] ...
-          </Cell>
-        </Grid>
-      </article>
-    </section>
-  )
+            <Cell>
+              Industries
+            </Cell>
+          </Grid>
+        </header>
+
+        { jobApps }
+
+      </section>
+    )
+  }
 }
 
-Dashboard.styles = {
+const styles = {
   section: {
     marginBottom: 20,
   },
